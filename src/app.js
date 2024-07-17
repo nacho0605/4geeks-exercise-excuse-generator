228ca53c2d5f76c92925ff27103bc25e1e9ec4f6
nacho0605 <carlos.colombana@gmail.com>
2024-07-16 23:31:27 -0300

    Exercise solution

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let excuse = `${randomElementFromArray(who)} ${randomElementFromArray(
+    action
+  )} ${randomElementFromArray(what)} ${randomElementFromArray(when)}`;
+  document.getElementById("excuse").innerHTML = excuse;
+  
   console.log("Hello Rigo from the console!");
 };
+
+function randomElementFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
